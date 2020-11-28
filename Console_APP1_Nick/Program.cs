f307b55b14d5485cce99e71c56810c51cf920a34
Nickolas <Nickolas@NICK>
2020-11-28 10:59:58 +0200

    Version_1_Commit

--- Console_APP1_Nick/Program.cs
+++ Console_APP1_Nick/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Console_APP1_Nick
 {
@@ -6,7 +7,39 @@ namespace Console_APP1_Nick
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Make a choice: \n 1. Create new account; \n 2. Get info about my account \n 3. Exit ");
+            Console.WriteLine("Input choice functions number");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            while (choice != 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+
+                        int newAccount = rnd.Next();
+                        Console.WriteLine($"New account number is: {newAccount}");
+                        break;
+                    case 2:
+                        Dictionary<int, string> accountdata = new Dictionary<int, string>(5);
+
+                        foreach (KeyValuePair<int, string> keyValue in accountdata)
+                        {
+                            accountdata.Add(rnd.Next(), "Person" + keyValue);
+                        }
+                        Console.WriteLine("Accounts Numbers and its owners");
+                        foreach (KeyValuePair<int, string> keyValue in accountdata)
+                        {
+                            Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+
+                        }
+
+                        break;
+                }
+            }
+
         }
+
+        
     }
 }
