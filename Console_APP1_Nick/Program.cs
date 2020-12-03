366a02fa14fff08288d0b12238cb3961d4f1698d
Nikola Rusanov <nikolarusano92@gmail.com>
2020-12-03 21:55:59 +0200

    Commit_fixed_programm_V_1_5

--- Console_APP1_Nick/Program.cs
+++ Console_APP1_Nick/Program.cs
@@ -11,8 +11,16 @@ namespace Console_APP1_Nick
             Console.WriteLine("Input choice functions number");
             int choice = Convert.ToInt32(Console.ReadLine());
             Random rnd = new Random();
-            while (choice != 3)
-            {
+
+            Dictionary<int, string> accountdata = new Dictionary<int, string>(5);
+            accountdata.Add(rnd.Next(), "Person1");
+            accountdata.Add(rnd.Next(), "Person2");
+            accountdata.Add(rnd.Next(), "Person3");
+            accountdata.Add(rnd.Next(), "Person4");
+            accountdata.Add(rnd.Next(), "Person5");
+            while (choice != 3){
+               
+                
                 switch (choice)
                 {
                     case 1:
@@ -21,13 +29,12 @@ namespace Console_APP1_Nick
                         Console.WriteLine($"New account number is: {newAccount}");
                         break;
                     case 2:
-                        Dictionary<int, string> accountdata = new Dictionary<int, string>(5);
-
-                        foreach (KeyValuePair<int, string> keyValue in accountdata)
-                        {
-                            accountdata.Add(rnd.Next(), "Person" + keyValue);
-                        }
-                        Console.WriteLine("Accounts Numbers and its owners");
+                        
+                        //foreach (KeyValuePair<int, string> keyValue in accountdata)
+                        //{
+                        //    accountdata.Add(rnd.Next(), "Person" + keyValue);
+                        //}
+                        Console.WriteLine("Accounts Numbers and its owners:");
                         foreach (KeyValuePair<int, string> keyValue in accountdata)
                         {
                             Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
@@ -38,10 +45,13 @@ namespace Console_APP1_Nick
                         Console.WriteLine("Inncorrect input, please input 1,2 or 3 value");
                         break;
                 }
+                Console.WriteLine("Input choice functions number");
+                choice = Convert.ToInt32(Console.ReadLine());
             }
 
         }
 
-        
+
     }
+
 }
